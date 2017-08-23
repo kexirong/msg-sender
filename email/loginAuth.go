582e9a671aa8e88c2330deab99deb91422aa9c84
kexirong <kexirong@outlook.com>
2017-08-23 09:38:20 +0800

    commit

---
+++ email/loginAuth.go
@@ -0,0 +1,35 @@
+package email
+
+import (
+    "strings"
+    "fmt"
+    "net/smtp"
+)
+
+type loginAuth struct {
+    username, password string
+}
+
+func LoginAuth(username, password string) smtp.Auth {
+    return &loginAuth{username, password}
+}
+func (a *loginAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {    
+    return "LOGIN", nil, nil
+}
+func (a *loginAuth) Next(fromServer []byte, more bool) ([]byte, error) {
+    command := string(fromServer)
+    command = strings.TrimSpace(command)
+    command = strings.TrimSuffix(command, ":")
+    command = strings.ToLower(command)
+    if more {
+        if (command == "username") {
+            return []byte(fmt.Sprintf("%s", a.username)), nil
+        } else if (command == "password") {
+            return []byte(fmt.Sprintf("%s", a.password)), nil
+        } else {
+            // We've already sent everything.
+            return nil, fmt.Errorf("unexpected server challenge: %s", command)
+        }
+    }
+    return nil, nil
+}
